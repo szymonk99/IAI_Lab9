91468ff36d46ec1e1fa85ea9ef0120d1005aacda
szymonk99 <72567307+szymonk99@users.noreply.github.com>
2021-01-13 14:02:06 +0100

    Add files via upload

---
+++ app/blog/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogItemComponent } from './components/blog-item/blog-item.component';
+import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
+import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
+import { DataService } from "./services/data.service";
+import { BlogComponent } from './components/blog/blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemImageComponent,
+    BlogItemDetailsComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    DataService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+function test(a, b) {
+    return a + b;
+}
+var wynik = test(4, 5);
+console.log(wynik);
